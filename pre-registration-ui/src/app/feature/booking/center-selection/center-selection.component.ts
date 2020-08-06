21edbcc425f65c01fffcd4b9c4732da23a693eb3
Elhadj KEITA <64079842+elhadj21@users.noreply.github.com>
2020-08-07 00:25:48 +0200

    clean code

--- pre-registration-ui/src/app/feature/booking/center-selection/center-selection.component.ts
+++ pre-registration-ui/src/app/feature/booking/center-selection/center-selection.component.ts
@@ -83,8 +83,6 @@ export class CenterSelectionComponent extends BookingDeactivateGuardService impl
   }
 
   getRecommendedCenters() {
-    console.log("this.users");
-    console.log(this.users);
     let prefectures = [];
     this.users.forEach((user) => {
       prefectures.push(user.location);
