a4e1cce6a8f000856abcfcb034e5bd39e893459f
Ankit <ankitvaishnav.iitb1991@gmail.com>
2020-08-06 16:31:04 +0530

    solving conflits with ank dev

--- pre-registration-ui/src/app/shared/models/demographic-model/name-list.modal.ts
+++ pre-registration-ui/src/app/shared/models/demographic-model/name-list.modal.ts
@@ -1,6 +1,9 @@
 export interface NameList {
   preRegId: string;
   fullName: string;
+  firstName: string;
+  lastName: string;
+  location: string;
   regDto?: any;
   status?: string;
   registrationCenter?: any;
