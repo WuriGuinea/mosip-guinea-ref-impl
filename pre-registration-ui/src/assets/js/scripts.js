0ffc4045489dcb110c8ad63519510658c3cea432
Mamadou Yaya DIALLO <myaya.diallo@wuriguinee.com>
2020-11-24 08:52:23 +0000

    js code refactored

---
+++ pre-registration-ui/src/assets/js/scripts.js
@@ -0,0 +1,21 @@
+window.cookieconsent.initialise({
+    "palette": {
+        "popup": {
+            "background": "#bce0fd",
+            "text": "#020f59"
+        },
+        "button": {
+            "background": "#020f59"
+        }
+    },
+    "theme": "classic",
+    "position": "bottom-left",
+    "type": "opt-out",
+    "content": {
+        close: '&#10005;',
+        message: "Nous utilisons des cookies pour vous garantir la meilleure experience sur notre site web. Si vous continuez a utiliser ce site, nous supposerons que vous en etes satisfait.",
+        dismiss: "OK",
+        link: " ",
+        "deny": "&#10005;",
+    },
+});
