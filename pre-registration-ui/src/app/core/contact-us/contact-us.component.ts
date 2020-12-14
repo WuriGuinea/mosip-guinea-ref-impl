95ee3548882b1a38b24f8e13444a1937492fccd5
mombe090 <yayamombeya090@gmail.com>
2020-12-14 17:26:12 +0000

    mail acknologement correction

--- pre-registration-ui/src/app/core/contact-us/contact-us.component.ts
+++ pre-registration-ui/src/app/core/contact-us/contact-us.component.ts
@@ -72,7 +72,7 @@ export class ContactUsComponent implements OnInit {
 
   openDialog(dialogType: DialogData): void {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '550px',
+      width: '310px',
       disableClose: true,
       data: dialogType
     });
@@ -165,7 +165,7 @@ export class ContactUsComponent implements OnInit {
         error => {
           console.log(error);
           this.invalidLogin = true;
-          this.dialogType.case = 0;
+          this.dialogType.case = 1;
 
           this.openDialog(this.dialogType);
         });
